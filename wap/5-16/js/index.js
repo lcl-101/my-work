0610aeac8f7ccaaf6bc2a5634ccbde029421ea16
lichenglong <lichenglong@wangcaigu.com>
2016-05-18 19:49:28 +0800

    会员等级

---
+++ wap/5-16/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    FastClick.attach(document.body);
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
+})
