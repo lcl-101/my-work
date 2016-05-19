6bc9f331bb042948400e608fd3bb387e6e94cacf
lichenglong <lichenglong@wangcaigu.com>
2016-05-19 19:06:14 +0800

    会员等级

--- wap/5-16/js/index.js
+++ wap/5-16/js/index.js
@@ -1,10 +1,23 @@
 $(function(){
     FastClick.attach(document.body);
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
+    var swiper = new Swiper('.swiper-container1', {
+        initialSlide:0,
         slidesPerView: 4,
-        centeredSlides: true,
-        paginationClickable: true,
         spaceBetween: 30
     });
-})
+    var sw=$('.swiper-container1 .swiper-slide').eq(0).width()+30;
+    $('.swiper-container1 .swiper-slide').on('click',function(){
+        var index=$('.swiper-container1 .swiper-slide').index(this);
+        $('.top-jiao').css({transform:'translateX('+sw*index+'px)'});
+    });
+    $('.swiper-container1 .swiper-slide img').on('click',function(){
+        var index=$('.swiper-container1 .swiper-slide').index(this);
+        $('.top-jiao').css({transform:'translateX('+sw*index+'px)'});
+    });
+
+    var swipers = new Swiper('.swiper-container2', {
+        initialSlide:0,
+        slidesPerView: 3,
+        spaceBetween: 30
+    });
+});
