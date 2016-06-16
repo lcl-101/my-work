3222b023cdd3ed7012d3aeb3fb4d427ef81a88dd
lichenglong <554675188@qq.com>
2016-06-16 14:06:44 +0800

    易保全

--- pc/6-16/js/index.js
+++ pc/6-16/js/index.js
@@ -5,5 +5,24 @@ $(function(){
     $('.bannerPop-close').on('click',function(){
         $('.bannerTitle-inner').fadeOut();
     });
-
+    $('.contrast-save').hover(function(){
+        $('.contrast-save img')[0].src='images/save-hover.png';
+    },function(){
+        $('.contrast-save img')[0].src='images/save.png';
+    });
+    $('.contrast-secret').hover(function(){
+        $('.contrast-secret img')[0].src='images/secret-hover.png';
+    },function(){
+        $('.contrast-secret img')[0].src='images/secret.png';
+    });
+    $('.contrast-authority').hover(function(){
+        $('.contrast-authority img')[0].src='images/authority-hover.png';
+    },function(){
+        $('.contrast-authority img')[0].src='images/authority.png';
+    });
+    $('.contrast-legal').hover(function(){
+        $('.contrast-legal img')[0].src='images/legal-hover.png';
+    },function(){
+        $('.contrast-legal img')[0].src='images/legal.png';
+    });
 })
