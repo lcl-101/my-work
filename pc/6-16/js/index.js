2b36869f32c47e9bcdc0f400694fbc01799fcfa9
lichenglong <554675188@qq.com>
2016-06-17 11:22:59 +0800

    易保全

--- pc/6-16/js/index.js
+++ pc/6-16/js/index.js
@@ -7,21 +7,29 @@ $(function(){
     });
     $('.contrast-save').hover(function(){
         $('.contrast-save img')[0].src='images/save-hover.png';
+        $('.contrast-right-inner p').html('安全');
+        $('.contrast-right-inner div').html('对客户合同内容进行保全，为投资者带来安全感');
     },function(){
         $('.contrast-save img')[0].src='images/save.png';
     });
     $('.contrast-secret').hover(function(){
         $('.contrast-secret img')[0].src='images/secret-hover.png';
+        $('.contrast-right-inner p').html('保密');
+        $('.contrast-right-inner div').html('系统仅在本地生成数据的数 字摘要，绝无泄露隐私、商 业秘密、内容的风险');
     },function(){
         $('.contrast-secret img')[0].src='images/secret.png';
     });
     $('.contrast-authority').hover(function(){
         $('.contrast-authority img')[0].src='images/authority-hover.png';
+        $('.contrast-right-inner p').html('权威');
+        $('.contrast-right-inner div').html('出具公证处公证及司法鉴定 中心鉴定，提供有效的法律 支持');
     },function(){
         $('.contrast-authority img')[0].src='images/authority.png';
     });
     $('.contrast-legal').hover(function(){
         $('.contrast-legal img')[0].src='images/legal-hover.png';
+        $('.contrast-right-inner p').html('合法');
+        $('.contrast-right-inner div').html('通过独立的第三方公证，提 高了电子合同的法律效力');
     },function(){
         $('.contrast-legal img')[0].src='images/legal.png';
     });
@@ -43,5 +51,23 @@ $(function(){
         $('.determine-left div').eq(index).addClass('determine-blue');
         $('.determine-right ul').hide();
         $('.determine-right ul').eq(index).show();
+        if(index==0){
+            $('.determine-btn').hide();
+        }else if(index==1){
+            $('.determine-btn').show();
+        }
+    });
+
+    $('.determine-btn div').hover(function(){
+        var index=$('.determine-btn div').index(this);
+        $('.determine-btn div').removeClass('determine-blue');
+        $('.determine-btn div').eq(index).addClass('determine-blue');
+        if(index==0){
+            $('.zs1').show();
+            $('.zs2').hide();
+        }else if(index==1){
+            $('.zs2').show();
+            $('.zs1').hide();
+        }
     })
 })
