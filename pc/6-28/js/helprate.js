13301992d09b132724296c1a96316bb3c3cd26ae
lichenglong <554675188@qq.com>
2016-06-29 11:35:47 +0800

    理财列表

--- pc/6-28/js/helprate.js
+++ pc/6-28/js/helprate.js
@@ -1,7 +1,17 @@
 $(function(){
-    $('.quan-click').eq(0).attr('src','images/down.png');
-    $('.quan-click').eq(0).attr('data-img','false');
-    $('.quan-inner').eq(0).stop(true,false).slideDown();
+    var search = window.location.search;
+    if (search.indexOf('type') != -1) {
+        var scrollArr = search.split('=');
+        $('html,body').animate({'scrollTop': $('.quan-box-content li').eq(scrollArr[1]).offset().top},500,function(){
+            $('.quan-click').eq(scrollArr[1]).attr('src','images/down.png');
+            $('.quan-click').eq(scrollArr[1]).attr('data-img','false');
+            $('.quan-inner').eq(scrollArr[1]).stop(true,false).slideDown();
+        });
+    }else {
+        $('.quan-click').eq(0).attr('src','images/down.png');
+        $('.quan-click').eq(0).attr('data-img','false');
+        $('.quan-inner').eq(0).stop(true,false).slideDown();
+    }
     $('.quan-title').on('click',function(){
         var index=$('.quan-title').index(this);
         for(var i=0;i<=$('.quan-click').length;i++){
