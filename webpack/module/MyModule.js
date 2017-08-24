757d0929d9400e179e6bdfb460f50d97112402c5
lichenglong <554675188@qq.com>
2017-08-24 11:43:56 +0800

    web pack

---
+++ webpack/module/MyModule.js
@@ -0,0 +1,11 @@
+// ES6 相对路径
+import utils from './../utils.js';
+
+// ES6 绝对路径
+import utils from '/utils.js';
+
+// CommonJS 相对路径
+var utils = require('./../utils.js');
+
+// CommonJS 绝对路径
+var utils = require('/utils.js');
